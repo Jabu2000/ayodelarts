b0365b510d280db195227c5c0e14ada69a5dcc78
Jabulani <joabjabulanilongwe@gmail.com>
2026-03-02 23:58:33 +0200

    reload-commit

--- ayodelarts/src/App.jsx
+++ ayodelarts/src/App.jsx
@@ -5,26 +5,47 @@ import Portfolio from "./page/Portfolio";
 import Contact from "./page/Contact";
 import Footer from "./components/Footer";
 import { AnimatePresence } from "framer-motion";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import Lenis from "lenis";
+import ScrollToTop from "./components/ScrollTop";
 
 function App() {
   const location = useLocation();
 
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.05,
+      smoothWheel: true,
+    });
+
+    // expose lenis globally
+    window.lenis = lenis;
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+      delete window.lenis;
+    };
+  }, []);
+
   return (
     <div>
-      <>
-        <AnimatePresence mode="wait">
-          <Navbar />
-          <Routes location={location} key={location.pathname}>
-            <Route index element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/footer" element={<Footer />} />
-          </Routes>
-        </AnimatePresence>
-      </>
+      <ScrollToTop />
+      <Navbar />
+      <Routes location={location} key={location.pathname}>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/footer" element={<Footer />} />
+      </Routes>
     </div>
   );
 }
