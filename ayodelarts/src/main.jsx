2bf75d82185ea13f85dd7b56819b1a4975b58a55
Jabulani <joabjabulanilongwe@gmail.com>
2026-03-02 23:22:45 +0200

    new-commit

--- ayodelarts/src/main.jsx
+++ ayodelarts/src/main.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import { StrictMode } from "react";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(  
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>,
 );
